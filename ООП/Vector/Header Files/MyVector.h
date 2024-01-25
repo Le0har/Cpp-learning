d2409d91d13314bc558beaa0083d835bce2c7736
Aleksey Tikhonov <142827000+Le0har@users.noreply.github.com>
2024-01-25 09:08:53 +0300

    MyVector.h

--- ООП/Vector/Header Files/MyVector.h
+++ ООП/Vector/Header Files/MyVector.h
@@ -1,28 +1,3 @@
 #pragma once
 #include <iostream>
-
-template <typename T>
-class MyVector{
-public:
-   int size;
-   int capacity;
-   T* array = nullptr;
-   
-   MyVector();                                                                  //пустой вектор
-   
-   MyVector(int size);                                                          //вектор состоит из size чисел, каждое равно 0
-   
-   MyVector(int size, T value);                                                 //вектор состоит из size чисел, каждое равно value
-   
-   void show();
-   
-   void push_back(T value);
-   
-   void pop_back();
-   
-   void insert(int index, T value);
-   
-   void remove(int index);
-   
-   ~MyVector();
-};
+#include "MyVector.tpp"
