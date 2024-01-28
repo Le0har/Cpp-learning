a1feb2f040e109e7fcbffad0ce39bcdc8b8f8ccc
Aleksey Tikhonov <142827000+Le0har@users.noreply.github.com>
2024-01-28 22:04:07 +0300

    Dog.h

---
+++ ООП/21 Exceptions/Header Files/Dog.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Tamagochi.h"
+
+class Dog: public Tamagochi {
+
+public:
+   static int countDog;
+
+   void voice() override;
+   
+   void play() override;
+  
+   void chewBone();
+   
+   Dog();
+
+   Dog(std::string name, int age, std::string breed);
+  
+   Dog(const Dog& d);
+  
+   Dog& operator=(const Dog& d);
+  
+   ~Dog();
+};
