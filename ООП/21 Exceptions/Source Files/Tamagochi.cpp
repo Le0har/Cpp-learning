3ec3e3ab8403b233faeb78cf0aa6160b8167655a
Aleksey Tikhonov <142827000+Le0har@users.noreply.github.com>
2024-01-28 22:07:55 +0300

    Tamagochi.cpp

---
+++ ООП/21 Exceptions/Source Files/Tamagochi.cpp
@@ -0,0 +1,35 @@
+#include "Tamagochi.h"
+
+void Tamagochi::addToy(int color, std::string shape)
+{
+   if (shape == name)
+   {
+      throw "Форма игрушки совпадает с именем животного!";
+   }
+   
+   switch (color)
+   {
+   case 1:     favoriteToys.push_back(Toy{ red, shape});
+               break;
+   case 2:     favoriteToys.push_back(Toy{ green, shape });
+               break;
+   case 3:     favoriteToys.push_back(Toy{ blue, shape });
+               break;
+   default:    std::cout << "Такого цвета нету!" << std::endl;
+   }
+}
+
+   void Tamagochi::setSound(std::string mysound)
+   {
+      sound = mysound;
+   }
+
+   Tamagochi::Tamagochi(std::string name = "Нет имени", int age = 0, std::string breed = "Нет породы",
+      std::string sound = " "):name(name), age(age), breed(breed)
+   {
+      setSound(sound);
+   }
+
+   Tamagochi::~Tamagochi()
+   {
+   }
