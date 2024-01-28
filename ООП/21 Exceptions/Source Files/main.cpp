828fa76f212d340d6c58282037443f3653000a3e
Aleksey Tikhonov <142827000+Le0har@users.noreply.github.com>
2024-01-28 22:08:41 +0300

    main.cpp

---
+++ ООП/21 Exceptions/Source Files/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Tamagochi.h"
+#include "Cat.h"
+#include "Dog.h"
+
+int Cat::countCat = 0;
+int Dog::countDog = 0;
+
+int main() {
+   setlocale(LC_ALL, "Rus");
+   try
+   {
+      Cat murka("Мурка", 4, "Британская короткошёрстная");
+
+      std::cout << "Имя: " << murka.name << std::endl;
+      std::cout << "Возраст: " << murka.age << std::endl;
+      std::cout << "Порода: " << murka.breed << std::endl;
+      murka.voice();
+
+      Cat barsik;
+
+      std::cout << "Имя: " << barsik.name << std::endl;
+      std::cout << "Возраст: " << barsik.age << std::endl;
+      std::cout << "Порода: " << barsik.breed << std::endl;
+      barsik.voice();
+
+      std::cout << "Количество кошек = " << Cat::countCat << std::endl;
+
+      murka.play();
+      barsik.play();
+
+      Dog reks("Рэкс", 7, "Немецкая овчарка");
+      reks.chewBone();
+      reks.addToy(1, "Круглый");
+      reks.play();
+      reks.addToy(1, "Рэкс");
+   }
+   catch (const char* errorMessage)
+   {
+      std::cout << errorMessage << std::endl;
+   }
+   
+   return 0;
+}
